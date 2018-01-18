31fac97b18679921b0cb842261665a7846f6c8b0
unknown <jhumphrey321@gmail.com>
2018-01-18 17:32:37 -0500

    Basic functionality is intact

---
+++ Bot_Template.py
@@ -0,0 +1,41 @@
+import discord
+import asyncio
+from discord.ext.commands import Bot
+from discord.ext import commands
+import platform
+
+client = Bot(description="BotBoy is hungry for 1's and 0's", command_prefix="!", pm_help = True)
+
+# This is what happens everytime the bot launches. In this case, it prints information like server count, user count the bot is connected to, and the bot id in the console.
+# Do not mess with it because the bot can break, if you wish to do so, please consult me or someone trusted.
+@client.event
+async def on_ready():
+	print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
+	print('--------')
+	print('Current Discord.py Version: {} | Current Python Version: {}'.format(discord.__version__, platform.python_version()))
+	print('--------')
+	print('Use this link to invite {}:'.format(client.user.name))
+	print('https://discordapp.com/oauth2/authorize?client_id={}&scope=bot&permissions=8'.format(client.user.id))
+	print('--------')
+	print('Support Discord Server: https://discord.gg/FNNNgqb')
+	print('Github Link: https://github.com/Habchy/BasicBot')
+	print('--------')
+	print('Created by RJ (Assistant to the Regional Software Engineer)')
+
+# This is a basic example of a call and response command. You tell it do "this" and it does it.
+@client.command()
+async def ping(*args):
+
+	await client.say(":ping_pong: Pong!")
+# After you have modified the code, feel free to delete the line above (line 33) so it does not keep popping up everytime you initiate the ping commmand.
+
+client.run('NDAzNjcxNTA2MTcwOTM3MzQ0.DUKwpw.bBisFKu9QeZDI_4TEO0d2e40jG8')
+
+# Basic Bot was created by Habchy#1665
+# Please join this Discord server if you need help: https://discord.gg/FNNNgqb
+# Please modify the parts of the code where it asks you to. Example: The Prefix or The Bot Token
+# This is by no means a full bot, it's more of a starter to show you what the python language can do in Discord.
+# Thank you for using this and don't forget to star my repo on GitHub! [Repo Link: https://github.com/Habchy/BasicBot]
+
+# The help command is currently set to be Direct Messaged.
+# If you would like to change that, change "pm_help = True" to "pm_help = False" on line 9.
